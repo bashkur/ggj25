782140f089d266db12f43efb16833089c462ebf8
Bash Kurgunayev <sebastiankurg@gmail.com>
2025-01-25 21:58:50 -0500

    attempt to undo previous 2 commits. fingers crossed.

---
+++ Assets/Scripts/Gameplay contollers/BubbleSpawnerScript.cs
@@ -0,0 +1,142 @@
+using UnityEngine;
+using System.Collections;
+
+public class BubbleSpawnerScript : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    
+    float timer;
+    int seconds;
+    int minutes;
+    int currentMinute = 1;
+    public float difficultyScalingMultiplier;
+    public float universalSpawnDelaySeconds = 3f;
+    public GameObject bubble;
+    public GameObject powerUpBubble;
+    public GameObject type2Bubble;
+    public GameObject type3Bubble;
+    GameObject bubbleType;
+
+    //Default as 1x
+
+    public float initialSpawnCredits = 50;
+    float maxSpawnCredits = 50;
+    int spawnCredits = 40;
+    public Vector3 ExcludeSpawnAreaXZ;
+    //Would declare what area around the player bubbles cannot spawn too close to player
+    Vector3 playerPosition;
+    float randomX;
+    float randomZ;
+    
+    
+    
+    public double normalBubbleWeight = 0.75;
+    public double powerUpBubbleWeight = 0.05;
+    public double Type2BubbleWeight = 0.1;
+    public double Type3BubbleWeight = 0.1;
+
+    bool spawnOnCooldown = false;
+
+    void Start()
+    {
+        randomPositionGenerator();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;   
+
+        seconds = (int)timer;
+        //Debug.Log("current second " + seconds);
+        //Debug.Log("current minute " + currentMinute);
+        if(seconds % 60 == 0 & seconds/60 == currentMinute & seconds != 0)
+        {
+            difficultyScaler();
+            currentMinute += 1;
+        }
+        if (spawnCredits <= (maxSpawnCredits) & spawnCredits >= 0 & spawnOnCooldown == false)
+        {
+            StartCoroutine(spawner());
+        }
+    }
+
+    GameObject bubbleTypeGenerator()
+    {
+        float random = Random.Range(0f,1f);
+        if(random <= normalBubbleWeight)
+        {
+            bubbleType = bubble;
+            spawnCredits -= 1;
+        }
+        if(random <= normalBubbleWeight)
+        {
+            bubbleType = powerUpBubble;
+            spawnCredits -= 5;
+        }
+        else if (random <= (normalBubbleWeight + powerUpBubbleWeight))
+        {
+            bubbleType = type2Bubble;
+            spawnCredits -= 5;
+        }
+        else if (random <= normalBubbleWeight + (normalBubbleWeight + powerUpBubbleWeight + Type2BubbleWeight))
+        {
+            bubbleType = type3Bubble;
+            spawnCredits -= 10;
+        }
+        return bubbleType;    
+    }
+
+    void difficultyScaler()
+    {
+        maxSpawnCredits = (maxSpawnCredits + 5) * difficultyScalingMultiplier;
+        maxSpawnCredits = Mathf.Floor(maxSpawnCredits);
+        if (universalSpawnDelaySeconds > 0)
+        {
+            universalSpawnDelaySeconds -= 0.2f;
+        }
+    }
+
+    IEnumerator spawner()
+    {
+       if (spawnCredits <= (maxSpawnCredits) & spawnOnCooldown == false)
+       {
+        Instantiate(bubbleTypeGenerator(), randomPositionGenerator(), Quaternion.identity);
+        spawnOnCooldown = true;
+        randomPositionGenerator();
+        yield return new WaitForSeconds(universalSpawnDelaySeconds);
+        spawnOnCooldown = false;
+       }
+       
+
+    }
+
+    Vector3 randomPositionGenerator()
+    {
+        randomX = Random.Range(ExcludeSpawnAreaXZ.x,-ExcludeSpawnAreaXZ.x); 
+        randomZ = Random.Range(ExcludeSpawnAreaXZ.z,-ExcludeSpawnAreaXZ.z);
+        const float Y = 0.5f;
+
+        if (randomX >= 0)
+        {
+            randomX = (Random.Range((ExcludeSpawnAreaXZ.x + 2),ExcludeSpawnAreaXZ.x) + ExcludeSpawnAreaXZ.x); 
+        }
+        else
+        {
+            randomX = (Random.Range((-ExcludeSpawnAreaXZ.x - 2),-ExcludeSpawnAreaXZ.x) - ExcludeSpawnAreaXZ.x); 
+        }
+
+        if (randomZ > 0)
+        {
+            randomZ = (Random.Range((ExcludeSpawnAreaXZ.z + 2),ExcludeSpawnAreaXZ.z) + ExcludeSpawnAreaXZ.z); 
+        }
+        else
+        {
+            randomZ = (Random.Range((ExcludeSpawnAreaXZ.z - 2),ExcludeSpawnAreaXZ.z) - ExcludeSpawnAreaXZ.z);
+        }
+
+        Vector3 RandomPosition = new Vector3((randomX + ExcludeSpawnAreaXZ.x), Y, randomZ);
+        Debug.Log(RandomPosition);
+        return RandomPosition;
+    }
+}
---
+++ Assets/Scripts/Gameplay contollers/bubbleController.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class bubbleController : MonoBehaviour
+{
+    Vector3 bubblePosition;
+    public float speed;
+    //Player = GameObject.Find("Player");
+    Vector3 playerPosition = new Vector3(0,0,0);
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        bubblePosition = new Vector3(this.transform.position.x, this.transform.position.y, this.transform.position.z);
+        this.transform.position = Vector3.MoveTowards(bubblePosition, (new Vector3(playerPosition.x,3,playerPosition.z)), (speed * Time.deltaTime));
+    }
+}
